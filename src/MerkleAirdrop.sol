8136da00e0e03aa9ab4eedd040f8460299cc36d6
Bonkesam <bonkesam9@gmail.com>
2025-03-19 07:52:10 +0300

    Adds the function getters

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -67,4 +67,12 @@ contract MerkleAirdrop is EIP712 {
         // transfer the tokens
         i_airdropToken.safeTransfer(account, amount);
     }
+
+    function getMerkleRoot() external view returns (bytes32) {
+        return i_merkleRoot;
+    }
+
+    function getAirdropToken() external view returns (IERC20) {
+        return i_airdropToken;
+    }
 }
